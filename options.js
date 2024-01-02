bcedb5cac918c9ee0e08cd64f75a867f0cca2a38
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2024-01-02 12:11:20 -0500

    WIP: ui refresh of the options page using bootstrap

--- options.js
+++ options.js
@@ -2,6 +2,43 @@
 // it deletes almost half of the file!!! So all the comments to not have it at the top!!!
 const REG_STRIP = /^[\r\t\f\v ]+|[\r\t\f\v ]+$/gm;
 
+// actions {{{
+const actions = {
+  advance: { name: 'advance', description: 'Advance' },
+  display: { name: 'display', description: 'Show/hide controller' },
+  fast: { name: 'fast', description: 'Preferred speed' },
+  faster: { name: 'faster', description: 'Increase speed' },
+  fixspeed_1: { name: 'fixspeed-1', description: '1x speed' },
+  fixspeed_1_5: { name: 'fixspeed-1.5', description: '1.5x speed' },
+  fixspeed_2: { name: 'fixspeed-2', description: '2x speed' },
+  fixspeed_2_5: { name: 'fixspeed-2.5', description: '2.5x speed' },
+  fixspeed_3: { name: 'fixspeed-3', description: '3x speed' },
+  fixspeed_3_5: { name: 'fixspeed-3.5', description: '3.5x speed' },
+  fixspeed_4: { name: 'fixspeed-4', description: '4x speed' },
+  fixspeed_4_5: { name: 'fixspeed-4.5', description: '4.5x speed' },
+  fixspeed_5: { name: 'fixspeed-5', description: '5x speed' },
+  fixspeed_5_5: { name: 'fixspeed-5.5', description: '5.5x speed' },
+  fixspeed_6: { name: 'fixspeed-6', description: '6x speed' },
+  fixspeed_6_5: { name: 'fixspeed-6.5', description: '6.5x speed' },
+  fixspeed_7: { name: 'fixspeed-7', description: '7x speed' },
+  fixspeed_7_5: { name: 'fixspeed-7.5', description: '7.5x speed' },
+  fixspeed_8: { name: 'fixspeed-8', description: '8x speed' },
+  fixspeed_8_5: { name: 'fixspeed-8.5', description: '8.5x speed' },
+  fixspeed_9: { name: 'fixspeed-9', description: '9x speed' },
+  fixspeed_9_5: { name: 'fixspeed-9.5', description: '9.5x speed' },
+  go_start: { name: 'go-start', description: 'Jump to start' },
+  jump: { name: 'jump', description: 'Jump to mark' },
+  mark: { name: 'mark', description: 'Mark location' },
+  muted: { name: 'muted', description: 'Mute' },
+  pause: { name: 'pause', description: 'Pause' },
+  reset: { name: 'reset', description: 'Reset speed' },
+  rewind: { name: 'rewind', description: 'Rewind' },
+  slower: { name: 'slower', description: 'Decrease speed' },
+  vol_down: { name: 'vol-down', description: 'Decrease volume' },
+  vol_up: { name: 'vol-up', description: 'Increase volume' },
+};
+// }}}
+
 // tdDefaults {{{
 var tcDefaults = {
   blacklist: `www.instagram.com
@@ -15,26 +52,56 @@ var tcDefaults = {
   enabled: true, // default enabled
   forceLastSavedSpeed: false, //default: false
   keyBindings: [
-    { action: 'display', key: 86, value: 0, force: false, predefined: true }, // V
-    { action: 'slower', key: 222, value: 0.1, force: false, predefined: true }, // ;
-    { action: 'faster', key: 186, value: 0.1, force: false, predefined: true }, // '
-    { action: 'rewind', key: 37, value: 5, value2: 5, force: true, predefined: true }, // Left
-    { action: 'advance', key: 39, value: 5, value2: 5, force: true, predefined: true }, // Right
-    { action: 'vol-up', key: 38, value: 0.05, force: true, predefined: true }, // Up
-    { action: 'vol-down', key: 40, value: 0.05, force: true, predefined: true }, // Down
-    { action: 'reset', key: 82, value: 1, force: false, predefined: true }, // R
-    { action: 'fast', key: 71, value: 1.8, force: false, predefined: true }, // G
-    { action: 'pause', key: 49, value: 0, force: false, predefined: false }, // 1
-    { action: 'go-start', key: 48, value: 0, force: false, predefined: false }, // 0
-    { action: 'fixspeed-1', key: 49, value: 1, force: true, predefined: false }, // 1
-    { action: 'fixspeed-2', key: 50, value: 2, force: true, predefined: false }, // 2
-    { action: 'fixspeed-3', key: 51, value: 3, force: true, predefined: false }, // 3
-    { action: 'fixspeed-4', key: 52, value: 4, force: true, predefined: false }, // 4
-    { action: 'fixspeed-5', key: 53, value: 5, force: true, predefined: false }, // 5
-    { action: 'fixspeed-6', key: 54, value: 6, force: true, predefined: false }, // 6
-    { action: 'fixspeed-7', key: 55, value: 7, force: true, predefined: false }, // 7
-    { action: 'fixspeed-8', key: 56, value: 8, force: true, predefined: false }, // 8
-    { action: 'fixspeed-9', key: 57, value: 9, force: true, predefined: false }, // 9
+    { action: actions.display, force: false, key: 86, predefined: true, value: 0 }, // V
+    { action: actions.slower, force: false, key: 222, predefined: true, value: 0.1 }, // ;
+    { action: actions.faster, force: false, key: 186, predefined: true, value: 0.1 }, // '
+    { action: actions.vol_up, force: true, key: 38, predefined: true, value: 0.05 }, // Up
+    { action: actions.vol_down, force: true, key: 40, predefined: true, value: 0.05 }, // Down
+    { action: actions.reset, force: false, key: 82, predefined: true, value: 1 }, // R
+    { action: actions.fast, force: false, key: 71, predefined: true, value: 1.8 }, // G
+    { action: actions.pause, force: false, key: 49, predefined: false, value: 0 }, // 1
+    { action: actions.go_start, force: false, key: 48, predefined: false, value: 0 }, // 0
+    { action: actions.fixspeed_1, force: true, key: 49, predefined: false, value: 1 }, // 1
+    { action: actions.fixspeed_2, force: true, key: 50, predefined: false, value: 2 }, // 2
+    { action: actions.fixspeed_3, force: true, key: 51, predefined: false, value: 3 }, // 3
+    { action: actions.fixspeed_4, force: true, key: 52, predefined: false, value: 4 }, // 4
+    { action: actions.fixspeed_5, force: true, key: 53, predefined: false, value: 5 }, // 5
+    { action: actions.fixspeed_6, force: true, key: 54, predefined: false, value: 6 }, // 6
+    { action: actions.fixspeed_7, force: true, key: 55, predefined: false, value: 7 }, // 7
+    { action: actions.fixspeed_8, force: true, key: 56, predefined: false, value: 8 }, // 8
+    { action: actions.fixspeed_9, force: true, key: 57, predefined: false, value: 9 }, // 9
+    { action: actions.fixspeed_1_5, force: true, key: 49, predefined: false, value: 1 }, // shift+1
+    { action: actions.fixspeed_2_5, force: true, key: 50, predefined: false, value: 2 }, // shift+2
+    { action: actions.fixspeed_3_5, force: true, key: 51, predefined: false, value: 3 }, // shift+3
+    { action: actions.fixspeed_4_5, force: true, key: 52, predefined: false, value: 4 }, // shift+4
+    { action: actions.fixspeed_5_5, force: true, key: 53, predefined: false, value: 5 }, // shift+5
+    { action: actions.fixspeed_6_5, force: true, key: 54, predefined: false, value: 6 }, // shift+6
+    { action: actions.fixspeed_7_5, force: true, key: 55, predefined: false, value: 7 }, // shift+7
+    { action: actions.fixspeed_8_5, force: true, key: 56, predefined: false, value: 8 }, // shift+8
+    { action: actions.fixspeed_9_5, force: true, key: 57, predefined: false, value: 9 }, // shift+9
+    // "Special" ones that have more things!
+    {
+      action: actions.rewind,
+      force: true,
+      key: 37,
+      postValue2Text: 'secs )',
+      postValueText: '% of duration, ',
+      predefined: true,
+      preValueText: 'Min of (',
+      value2: 5,
+      value: 5,
+    }, // Left
+    {
+      action: actions.advance,
+      force: true,
+      key: 39,
+      postValue2Text: 'secs )',
+      postValueText: '% of duration, ',
+      predefined: true,
+      preValueText: 'Min of (',
+      value2: 5,
+      value: 5,
+    }, // Right
   ],
   logLevel: 3, // default: 3
   rememberSpeed: false, // default: false
@@ -42,6 +109,17 @@ var tcDefaults = {
   startHidden: false, // default: false
 };
 // }}}
+
+const bindingOptions = _.map(
+  actions,
+  ({ name, description }) => `<option value="${name}">${description}</option>`,
+);
+
+const getTcDefaultBinding = (action) => {
+  const toCompare = typeof action === 'string' ? action : action.name;
+
+  return _.find(tcDefaults.keyBindings, (b) => b.action.name === toCompare);
+}
 
 var keyBindings = [];
 
@@ -194,39 +272,112 @@ function forgetAll() {
 // customActionsNoValues {{{
 // List of custom actions for which customValue should be disabled
 var customActionsNoValues = [
-  'pause',
-  'go-start',
-  'muted',
-  'mark',
-  'jump',
-  'display',
-  'fixspeed-1',
-  'fixspeed-1.5',
-  'fixspeed-2',
-  'fixspeed-2.5',
-  'fixspeed-3',
-  'fixspeed-3.5',
-  'fixspeed-4',
-  'fixspeed-4.5',
-  'fixspeed-5',
-  'fixspeed-5.5',
-  'fixspeed-6',
-  'fixspeed-6.5',
-  'fixspeed-7',
-  'fixspeed-7.5',
-  'fixspeed-8',
-  'fixspeed-8.5',
-  'fixspeed-9',
-  'fixspeed-9.5',
+  actions.pause.name,
+  actions.go_start.name,
+  actions.muted.name,
+  actions.mark.name,
+  actions.jump.name,
+  actions.display.name,
+  actions.fixspeed_1.name,
+  actions.fixspeed_1_5.name,
+  actions.fixspeed_2.name,
+  actions.fixspeed_2_5.name,
+  actions.fixspeed_3.name,
+  actions.fixspeed_3_5.name,
+  actions.fixspeed_4.name,
+  actions.fixspeed_4_5.name,
+  actions.fixspeed_5.name,
+  actions.fixspeed_5_5.name,
+  actions.fixspeed_6.name,
+  actions.fixspeed_6_5.name,
+  actions.fixspeed_7.name,
+  actions.fixspeed_7_5.name,
+  actions.fixspeed_8.name,
+  actions.fixspeed_8_5.name,
+  actions.fixspeed_9.name,
+  actions.fixspeed_9_5.name,
 ];
+// }}}
+
+// add_predefined {{{
+function addBinding(item) {
+  const { action, predefined, value2 } = item;
+
+  const tcDefault = getTcDefaultBinding(action)
+
+  let valueHtml = `
+  <input class="customValue w-50" type="text" placeholder="value (0.10)" />
+`;
+
+  if (value2) {
+    valueHtml = `
+  <span>${item.preValueText || tcDefault.preValueText}</span>
+  <input class="customValue w-25" type="text" placeholder="value (${tcDefault.value})" />
+  <span>${item.postValueText || tcDefault.postValueText}</span>
+  <input class="customValue2 w-25" type="text" placeholder="value (${tcDefault.value2})" />
+  <span>${item.postValue2Text || tcDefault.postValue2Text}</span>
+`;
+  }
+
+  let removeBtn = '';
+  if (!predefined) {
+    removeBtn = '<button class="removeParent btn btn-danger">X</button>';
+  }
+
+  const html = `
+<tr id="${action}">
+  <td>
+    <select class="customDo form-select">
+      ${bindingOptions.join('\n')}
+    </select>
+  </td>
+  <td>
+    <div class="form-check">
+      <input type="checkbox" class="form-check-input" name="shift" /><label class="modifier form-check-label">SHIFT</label>
+    </div>
+    <div class="form-check">
+      <input type="checkbox" class="form-check-input" name="ctrl" /><label class="modifier form-check-label">CTRL</label>
+    </div>
+  </td>
+  <td>
+    <input class="customKey w-50" type="text" value="" placeholder="press a key" class="form-control" />
+  </td>
+  <td>
+    ${valueHtml}
+  </td>
+  <td>
+    <select class="customForce form-select">
+      <option value="false">Do not disable website key bindings</option>
+      <option value="true">Disable website key bindings</option>
+    </select>
+  </td>
+  <td>${removeBtn}</td>
+</tr>
+`;
+  const shortcuts = document.querySelector('#shortcuts tbody');
+  shortcuts.insertAdjacentHTML('beforeend', html);
+
+  const forceSelect = shortcuts.querySelector('tr:last-of-type .customForce');
+  forceSelect.addEventListener('change', (event) => {
+    const target = event.target;
+    const value = JSON.parse(target.value);
+
+    target.classList.remove('text-warning');
+    target.classList.remove('text-success');
+
+    const colorCls = value ? 'text-warning' : 'text-success';
+    target.classList.add(colorCls);
+  });
+}
+
 // }}}
 
 // add_shortcut {{{
 function add_shortcut() {
-  var html = `
+  const html = `
     <tr>
       <td>
-        <select class="customDo">
+        <select class="customDo form-select">
           <option value="slower">Decrease speed</option>
           <option value="faster">Increase speed</option>
           <option value="rewind">Rewind</option>
@@ -262,8 +413,12 @@ function add_shortcut() {
         </select>
       </td>
       <td>
-        <input type="checkbox" name="shift" /><label class="modifier">SHIFT</label>
-        <input type="checkbox" name="ctrl" /><label class="modifier">CTRL</label>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" name="shift" /><label class="modifier form-check-label">SHIFT</label>
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" name="ctrl" /><label class="modifier form-check-label">CTRL</label>
+        </div>
       </td>
       <td>
         <input class="customKey" type="text" placeholder="press a key"/>
@@ -272,13 +427,13 @@ function add_shortcut() {
         <input class="customValue" type="text" placeholder="value (0.10)"/>
       </td>
       <td>
-        <select class="customForce">
+        <select class="customForce form-select">
           <option value="false">Do not disable website key bindings</option>
           <option value="true">Disable website key bindings</option>
         </select>
       </td>
       <td>
-        <button class="removeParent">X</button>
+        <button class="removeParent btn btn-danger">X</button>
       </td>
     </tr>
 `;
@@ -302,7 +457,7 @@ function createKeyBindings(item) {
   const ctrl = item.querySelector('input[name="ctrl"]').checked;
 
   const value = Number(item.querySelector('.customValue').value);
-  const force = item.querySelector('.customForce').value;
+  const force = JSON.parse(item.querySelector('.customForce').value);
   const predefined = !!item.id; //item.id ? true : false;
 
   let binding = {
@@ -320,7 +475,7 @@ function createKeyBindings(item) {
     binding = {
       ...binding,
       value2: Number(value2El.value),
-    }
+    };
   }
 
   keyBindings.push(binding);
@@ -361,9 +516,9 @@ function validate() {
 }
 // }}}
 
-// save_options {{{
+// saveOptions {{{
 // Saves options to chrome.storage
-function save_options() {
+function saveOptions() {
   if (!validate()) {
     return;
   }
@@ -418,9 +573,9 @@ function save_options() {
 }
 // }}}
 
-// restore_options {{{
+// restoreOptions {{{
 // Restores options from chrome.storage
-function restore_options() {
+function restoreOptions() {
   chrome.storage.sync.get(tcDefaults, function (storage) {
     document.getElementById('audioBoolean').checked = storage.audioBoolean;
     document.getElementById('blacklist').value = storage.blacklist;
@@ -441,14 +596,45 @@ function restore_options() {
       });
     }
 
-    const keyBindings = _.sortBy(storage.keyBindings, (b) => b.action);
+    const keyBindings = _.sortBy(storage.keyBindings, (b) => b.action.description);
 
-    for (let i in keyBindings) {
-      var item = keyBindings[i];
-      const action = item['action'];
-      const tcDefault = _.find(tcDefaults.keyBindings, b => b.action === action);
+    for (let item of keyBindings) {
+      const action = item.action;
 
+      const tcDefault = getTcDefaultBinding(action)
+
+      addBinding(item);
+
+      const dom = document.querySelector('#shortcuts tbody tr:last-of-type');
+      dom.querySelector('.customDo').value = action;
+
+      if (customActionsNoValues.includes(action)) {
+        dom.querySelector('.customValue').style.display = 'none';
+      }
+
+      updateCustomShortcutInputText(dom.querySelector('.customKey'), item.key);
+      dom.querySelector('input[name="shift"]').checked = item.shift;
+      dom.querySelector('input[name="ctrl"]').checked = item.ctrl;
+
+      dom.querySelector('.customValue').value = item.value;
+
+      const force = JSON.parse(item.force);
+      dom.querySelector('.customForce').value = force;
+
+      const colorCls = force ? 'text-warning' : 'text-success';
+      dom.querySelector('.customForce').classList.add(colorCls);
+
+      if (item.value2 || tcDefault.value2) {
+        const customValue2 = dom.querySelector('.customValue2');
+        if (customValue2) customValue2.value = item.value2 || tcDefault.value2;
+      }
+
+      /*
       if (item.predefined) {
+        add_predefined({ action });
+        const dom = document.querySelector('#farzad tbody tr:last-of-type');
+        dom.querySelector('.customDo').value = action;
+
         //do predefined ones because their value needed for overlay
         // document.querySelector("#" + item["action"] + " .customDo").value = item["action"];
         if (action == 'display' && typeof item['key'] === 'undefined') {
@@ -474,7 +660,7 @@ function restore_options() {
         // new ones
         add_shortcut();
         // const dom = document.querySelector('.customs:last-of-type');
-        const dom = document.querySelector('#shortcuts tr:last-of-type');
+        const dom = document.querySelector('#shortcuts tbody tr:last-of-type');
         dom.querySelector('.customDo').value = action;
 
         if (customActionsNoValues.includes(action)) {
@@ -491,15 +677,23 @@ function restore_options() {
         const customValue2 = dom.querySelector('.customValue');
         if (customValue2) customValue2.value = item['value2'];
       }
+      */
+    }
+
+    const total = _.keys(actions).length;
+    const used = document.querySelectorAll('#shortcuts tbody tr').length;
+
+    if (used < total) {
+      document.querySelector('#add').style.display = '';
     }
   });
 }
 // }}}
 
-// restore_defaults {{{
-function restore_defaults() {
+// restoreDefaults {{{
+function restoreDefaults() {
   chrome.storage.sync.set(tcDefaults, function () {
-    restore_options();
+    restoreOptions();
     document.querySelectorAll('.removeParent').forEach((button) => button.click()); // Remove added shortcuts
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -600,11 +794,11 @@ function toggleDisplaySpeeds() {
 
 // DOMContentLoaded; entry point basically {{{
 document.addEventListener('DOMContentLoaded', function () {
-  restore_options();
+  restoreOptions();
 
-  document.getElementById('save').addEventListener('click', save_options);
-  document.getElementById('add').addEventListener('click', add_shortcut);
-  document.getElementById('restore').addEventListener('click', restore_defaults);
+  document.getElementById('save').addEventListener('click', saveOptions);
+  document.getElementById('add').addEventListener('click', addBinding);
+  document.getElementById('restore').addEventListener('click', restoreDefaults);
   document.getElementById('experimental').addEventListener('click', show_experimental);
   document.getElementById('forgetAll').addEventListener('click', forgetAll);
   document.getElementById('toggleDisplaySpeeds').addEventListener('click', toggleDisplaySpeeds);
