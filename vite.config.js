c002b2576b2e76d23a0f1976cc6b68b2693223d1
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2024-03-14 09:58:51 -0400

    Add eslint stuff!

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest-react.json';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [react(), crx({ manifest }), eslint()],
   server: {
     port: 5173,
     strictPort: true,
