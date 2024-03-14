0e10d3381a4665034d6fc16504456ea80fd8446e
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2024-03-13 22:36:21 -0400

    Match package.json and vite.config.js with a new vite-react project

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { crx } from '@crxjs/vite-plugin';
+import manifest from './manifest.json';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        optionsTest: resolve(__dirname, 'options-test.html'),
-      },
-    },
-  },
+  plugins: [react(), crx({ manifest })],
 });
