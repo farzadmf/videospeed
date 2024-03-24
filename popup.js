bec8b2412c49d57fb02c51a47d9b20654b2d9926
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2024-03-24 11:44:48 -0400

    Fix popup error for browserAction

--- popup.js
+++ popup.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#disable').classList.toggle('hide', !enabled);
 
     const suffix = `${enabled ? '' : '_disabled'}.png`;
-    chrome.browserAction.setIcon({
+    chrome.action.setIcon({
       path: {
         19: 'icons/icon19' + suffix,
         38: 'icons/icon38' + suffix,
@@ -50,9 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function settingsSavedReloadMessage(enabled) {
-    setStatusMessage(
-      `${enabled ? 'Enabled' : 'Disabled'}. Reload page to see changes`,
-    );
+    setStatusMessage(`${enabled ? 'Enabled' : 'Disabled'}. Reload page to see changes`);
   }
 
   function setStatusMessage(str) {
