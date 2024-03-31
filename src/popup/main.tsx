a3baf98fa12c494a9c5ccee543e9550e969d69c8
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2024-03-31 00:04:07 -0400

    Some playing around with the popup

--- src/popup/main.tsx
+++ src/popup/main.tsx
@@ -1,23 +1,25 @@
+import logo from '../../icons/icon-react-128.png';
+
 export const Popup = () => {
   return (
     <div className="container mx-auto min-h-[200px] min-w-[250px]">
-      <div className="my-10 grid grid-cols-1 items-center justify-items-center">
+      <div className="mx-5 my-10 flex flex-col items-center gap-4">
+        <div className="text-xl italic">Video UTILS</div>
+        <img src={logo} className="w-16" />
+        <div className="divider divider-primary my-1"></div>
         <button
-          className="btn btn-primary btn-sm my-5 w-8/12"
+          className="btn btn-primary btn-sm w-full"
           onClick={() => window.open(chrome.runtime.getURL('/src/options/index.html'))}
         >
           Options ...
         </button>
         <button
-          className="btn btn-primary btn-sm my-5 w-8/12"
+          className="btn btn-primary btn-sm w-full"
           onClick={() => window.open('https://github.com/farzadmf/videospeed/issues')}
         >
           Issues ...
         </button>
-        <button
-          className="btn btn-primary btn-sm my-5 w-8/12"
-          onClick={() => window.open('https://github.com/farzadmf/videospeed')}
-        >
+        <button className="btn btn-primary btn-sm w-full" onClick={() => window.open('https://github.com/farzadmf/videospeed')}>
           About ...
         </button>
       </div>
