30f4346209aff5058a9d760bb93169c48e024edb
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2025-07-16 00:04:11 -0400

    chore: use Website's URL to store speeds and not video's URL

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,10 +1,14 @@
-export function getBaseURL(fullURL) {
-  const urlReg = new RegExp(new RegExp(/(https?:\/\/.*?\/).*/));
-  const match = fullURL.match(urlReg);
+export function getBaseURL(_fullURL) {
+  // Decided to simply use the Website's URL as videos can have super weird URLs
+  //    because of CDN and such.
+  return location.hostname;
 
-  if (match) {
-    return match[match.length - 1];
-  }
-
-  return null;
+  // const urlReg = new RegExp(new RegExp(/(https?:\/\/.*?\/).*/));
+  // const match = fullURL.match(urlReg);
+  //
+  // if (match) {
+  //   return match[match.length - 1];
+  // }
+  //
+  // return null;
 }
