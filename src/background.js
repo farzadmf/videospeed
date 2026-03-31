54df85ad18f342ad8de600960aaddbcccde60e2d
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2026-03-31 17:59:40 -0400

    fix: [UPSTREAM] Remove activeTab permission; make background sole icon authority
    Remove redundant activeTab permission (already covered by
    content_scripts host permissions). Remove dual-writer pattern on
    toolbar icon: popup no longer calls setIcon or sends EXTENSION_TOGGLE
    message. Background owns icon state exclusively via storage.onChanged.
    
    https://github.com/igrigorik/videospeed/commit/c329647

--- src/background.js
+++ src/background.js
@@ -77,16 +77,6 @@ chrome.storage.onChanged.addListener((changes, namespace) => {
 });
 
 /**
- * Handle messages from popup
- */
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === 'EXTENSION_TOGGLE') {
-    // Update icon when extension is toggled via popup
-    updateIcon(message.enabled);
-  }
-});
-
-/**
  * Initialize on install/update
  */
 chrome.runtime.onInstalled.addListener(async () => {
